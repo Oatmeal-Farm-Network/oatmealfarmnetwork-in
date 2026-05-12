0bc91b28ab8e5827bc641ef2727d9f9943525999
OatmealAIJohn <livestockoftheworld@gmail.com>
2026-05-12 09:51:56 -0700

    modified:   oatmealfarmnetwork 	modified:   src/Directory/pages/DirectoryList.jsx 	modified:   src/Directory/pages/directoryMappings.jsx 	modified:   src/IngredientCategory.jsx 	modified:   src/IngredientKnowledgebase.jsx

--- src/Directory/pages/directoryMappings.jsx
+++ src/Directory/pages/directoryMappings.jsx
@@ -81,7 +81,7 @@ export const DIRECTORY_TYPE_TO_IMAGE = {
     'restaurants': restaurantsImg,
     'retailers': retailersImg,
     'service-providers': serviceProvidersImg,
-    'transporters': '/images/207851Transporter.webp',
+    'transporters': '/images/Transportation.webp',
     'universities': universitiesImg,
     'veterinarians': vetImg,
     'vineyards': vineyardsImg,
