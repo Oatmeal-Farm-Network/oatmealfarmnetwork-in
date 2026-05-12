78f232a8ab3b21f0f683b5358aea661f93b83e7d
OatmealAIJohn <livestockoftheworld@gmail.com>
2026-05-11 20:26:05 -0700

    modified:   src/AboutEvents.jsx 	modified:   src/AboutJobBoard.jsx 	modified:   oatmealfarmnetwork (modified content, untracked content)

--- src/AboutEvents.jsx
+++ src/AboutEvents.jsx
@@ -52,7 +52,7 @@ export default function AboutEvents() {
       <div className="mx-auto px-4 pt-2" style={{ maxWidth: '1300px' }}>
         <div className="relative w-full overflow-hidden rounded-xl">
           <img
-            src="/images/CoreFeaturesAssociationSupport.webp"
+            src="/images/EventBanner.webp"
             alt="Farm Events"
             className="w-full object-cover"
             style={{ height: '260px', display: 'block' }}
